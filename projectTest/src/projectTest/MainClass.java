87f84d3f594887327f40ce3db57269662fbeef11
hhhhicode <hhhhicode@gmail.com>
2020-06-30 05:11:58 +0900

    '깃 연습'

---
+++ projectTest/src/projectTest/MainClass.java
@@ -0,0 +1,9 @@
+package projectTest;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// 프로그램이 시작될 때 가장 먼저 main 부터 시작된다.
+		System.out.println("Hello Java World!!!");
+	}
+}
